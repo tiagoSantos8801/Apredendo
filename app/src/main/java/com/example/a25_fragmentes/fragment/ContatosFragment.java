bf8f3825771834fda447c7f3994d7dc357191389
Tiago Santos <thiagoXavierSist@gmail.com>
2020-11-30 14:56:57 -0300

    Primeiro Commit

--- app/src/main/java/com/example/a25_fragmentes/fragment/ContatosFragment.java
+++ app/src/main/java/com/example/a25_fragmentes/fragment/ContatosFragment.java
@@ -10,11 +10,7 @@ import android.view.ViewGroup;
 
 import com.example.a25_fragmentes.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ContatosFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ContatosFragment extends Fragment {
 
      // TODO: Rename parameter arguments, choose names that match
